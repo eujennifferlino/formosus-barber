43e42b86e0ab88cd1c84d2ff93df657fd338451f
eujennifferlino <eujennifferlino@gmail.com>
2024-02-04 23:08:00 -0300

    feat: add correction Header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
@@ -12,6 +10,17 @@ const Header = () => {
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
         <Image src="/Logo.png" alt="Formosus Barber" height={22} width={120} />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   );
